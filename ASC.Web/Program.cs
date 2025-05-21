2174a0d14d82ef34aff9f8479ba151fc251784a1
thanhliem121004 <2224802010267@student.tdmu.edu.vn>
2025-05-21 19:35:43 +0700

    Final

--- ASC.Web/Program.cs
+++ ASC.Web/Program.cs
@@ -15,7 +15,6 @@ ExcelPackage.License.SetNonCommercialPersonal("Auto mobile Service Center");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -23,7 +22,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
